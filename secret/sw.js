21e9c0f9cadbff8a3b498a7afaf1501b4fe9b381
Étienne Charignon <etienne.charignon@gmail.com>
2026-05-15 14:12:27 +0200

    🔧 Met en cache les ressources de la PWA

--- secret/sw.js
+++ secret/sw.js
@@ -1,5 +1,11 @@
-const CACHE = 'porte-v1';
-const ASSETS = ['/', '/index.html'];
+const CACHE = 'porte-v1.1';
+const ASSETS = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png',
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
